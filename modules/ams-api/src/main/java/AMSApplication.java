871b682a5b6d7cd98a95ffef5e33552f7b8172c4
vmduc2 <vmduc2@cmc.com.vn>
2022-05-10 16:15:18 +0700

    init

---
+++ modules/ams-api/src/main/java/AMSApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableAspectJAutoProxy
+public class AMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AMSApplication.class, args);
+    }
+}
