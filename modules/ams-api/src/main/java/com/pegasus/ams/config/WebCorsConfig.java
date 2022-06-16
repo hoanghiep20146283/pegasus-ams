59297d2c318f22e449b810316511495a9d6020ea
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2022-06-16 22:20:28 +0700

    Eliminate unnecessary security configurations

--- modules/ams-api/src/main/java/com/pegasus/ams/config/WebCorsConfig.java
+++ modules/ams-api/src/main/java/com/pegasus/ams/config/WebCorsConfig.java
@@ -1,15 +1,12 @@
 package com.pegasus.ams.config;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -19,10 +16,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebCorsConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private CorsConfigProperties corsConfigProperties;
+    private final CorsConfigProperties corsConfigProperties;
 
     private final Logger log = LoggerFactory.getLogger(WebCorsConfig.class);
 
